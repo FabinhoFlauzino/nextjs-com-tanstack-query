c961b91a1d32c58daf1124bb14fdf40b87e54e08
unknown <fabioflauzino@hotmail.com>
2024-09-25 00:56:48 -0300

    Criando paginação simples e usando prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
 "use client"
 
-import { usePost, usePosts } from "@/utils/queries";
+import { usePosts, useUsersPrefetch } from "@/utils/queries";
+import { useState } from "react";
 
 export default function Page() {
+  useUsersPrefetch()
+  const [page, setPage] = useState(0)
+  const limit = 10
+  let total = page * limit
 
-  const posts = usePosts()
-  const postItem = usePost(10)
+  const posts = usePosts(limit, total)
+
+  const handlePrevButton = () => {
+    setPage(page === 0 ? 0 : page - 1)
+  }
+
+  const handleNextButton = () => {
+    setPage(page + 1)
+  }
 
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl">Olá</h1>
+
+      <div className="p-3 m-3 border border-white">
+        <div>Items por página: {limit}</div>
+        <div>Número da página: {page}</div>
+        <button className="border mx-2 px-2" onClick={handlePrevButton}>Página Anterior</button>
+        <button className="border mx-2 px-2" onClick={handleNextButton}>Próxima Página</button>
+      </div>
 
       {posts.isLoading && 'carregando....'}
       {!posts.isLoading && posts.isFetching && 'carregando....'}
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,16 +1,25 @@
-import { useQuery } from "@tanstack/react-query"
-import { getPost, getPosts } from "./api"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { getPost, getPosts, getUsers } from "./api"
+import { postsInitialData } from "@/data/postInitialData"
 
-export const usePosts = () => {
-  const query = useQuery({ 
-    queryKey: ['posts'], 
-    queryFn: getPosts,
-    staleTime: Infinity
-   })
+export const usePosts = (limit: number, start: number) => {
+  const query = useQuery({
+    queryKey: ['posts', { limit, start }],
+    queryFn: () => getPosts(limit, start),
+    placeholderData: postsInitialData
+  })
   return query
 }
 
 export const usePost = (id: number) => {
   const query = useQuery({ queryKey: ['posts', id], queryFn: () => getPost(id) })
   return query
+}
+
+export const useUsersPrefetch = () => {
+  const queryClient = useQueryClient()
+  queryClient.prefetchQuery({
+    queryKey: ['users'],
+    queryFn: getUsers,
+  })
 }
